f4e87d429e910f64c9a64ff04621cb4abc280395
kyudong3 <kyudong3@gmail.com>
2019-01-19 12:52:35 +0900

    회원가입

---
+++ app/src/main/java/com/kyudong/netflixandroid/signup/SignUpFirstActivity.kt
@@ -0,0 +1,13 @@
+package com.kyudong.netflixandroid.signup
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.kyudong.netflixandroid.R
+
+class SignUpFirstActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up_first)
+    }
+}
